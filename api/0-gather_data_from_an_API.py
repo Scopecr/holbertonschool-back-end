cce82ca9be671bb04a525e4079a330a78032df00
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-12-20 10:42:40 -0400

    again

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,56 +1,52 @@
 #!/usr/bin/python3
 """Script to return info about todo list progress"""
 import requests
+from requests import get
 from sys import argv
 
 
 def information_employee():
     """Returns information about employees"""
-    if len(argv) != 2:
-        print("Usage: {} <employee_id>".format(argv[0]))
-        return
-
+    id_employee = int(argv[1])
     id_employee = int(argv[1])
     employee_name = ""
-    number_of_done_tasks = 0
-    total_number_of_tasks = 0
-    task_titles = []
+    number_of_done_task = 0
+    total_number_of_task = 0
+    task_title = []
 
     url_users = 'https://jsonplaceholder.typicode.com/users'
-    url_todos = 'https://jsonplaceholder.typicode.com/todos'
 
     # Get user data
     user_url = f"https://jsonplaceholder.typicode.com/users/{id_employee}"
     user_response = requests.get(user_url)
-
-    if user_response.status_code != 200:
-        print("Error: Unable to fetch user data. Status code: {}".format(user_response.status_code))
-        return
-
     user_data = user_response.json()
     employee_name = user_data['name']
 
-    # Get todos data
-    todos_response = requests.get(url_todos)
+    url_todos = 'https://jsonplaceholder.typicode.com/todos'
 
-    if todos_response.status_code != 200:
-        print("Error: Unable to fetch todos data. Status code: {}".format(todos_response.status_code))
-        return
+    response_one = get(url_users)
+    response_two = get(url_todos)
 
-    todos_data = todos_response.json()
+    if response_one.status_code == 200:
+        response_json_usr = response_one.json()
+        response_json_tod = response_two.json()
 
-    for todo in todos_data:
-        if todo['userId'] == id_employee:
-            total_number_of_tasks += 1
-            if todo['completed']:
-                number_of_done_tasks += 1
-                task_titles.append(todo['title'])
+        for user in response_json_usr:
+            if (user['id'] == id_employee):
+                employee_name = user['name']
 
-    print('Employee {} is done with tasks ({}/{}):'
-          .format(employee_name, number_of_done_tasks, total_number_of_tasks))
-    
-    for title in task_titles:
-        print('\t{}'.format(title))
+                for tod in response_json_tod:
+                    if tod['userId'] == id_employee:
+                        total_number_of_task += 1
+                        if tod['completed'] is True:
+                            number_of_done_task += 1
+                            task_title.append(tod['title'])
+
+        print('Employee {} is done with tasks({}/{}):'
+              .format(employee_name, number_of_done_task,
+                      total_number_of_task))
+        for title in task_title:
+            print('\t {}'.format(title))
 
 
 if __name__ == "__main__":
